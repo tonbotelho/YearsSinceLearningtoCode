f2436091f671a6e8fb3cad9aeb166c2374e82e19
Ieslen <ieslenb@gmail.com>
2018-11-01 19:20:06 +0000

    Update datasource.py
    Code updated to read the csv file.

--- datasource.py
+++ datasource.py
@@ -5,25 +5,19 @@
 # ------------------------------------------------------------------------
 
 # Import system librarys to manipulate the paths
-#os and sys = It is used to envirement manipulation
+#os and sys = It is used to manipulation
 
-import os, sys
+import os
+import csv
+import sys
+import pandas as pd
 
-datasource_name = 'survey_results_public.csv'
-# In Java
-# System.getProperty("user.dir") -> sys.path[1]
-# System.getProperty("file.separator") -> os.path
-# join has the responsability to concat strings of a list
-# join put the file separator between each comma of the list
+datasource_name = 'lecturer.csv'
 datasource_path = os.path.join(sys.path[1], 'database', datasource_name)
-# print(datasource_path)
-
-con = open(datasource_path , 'r')
-
-test = con.readline().split(',')
-# Arrange this csv into an dictionary based on ID so it can be handled.
-for i,t in enumerate(test):
-    print("%d, %s"%(i,t))
-
-
-# HTTP used to catch data for google grafics
+print(datasource_path)
+datasource_file = open(datasource_path, 'r', encoding="utf8") #open file on read mode
+datasource_file.readline()
+reader = csv.reader(datasource_file, delimiter=',')
+columns = [0,5,6]
+for r in reader:
+    print([r[i] for i in columns])
